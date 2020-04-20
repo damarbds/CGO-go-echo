49f0c7d4ec039535b178dfccb27c40356e06e241
User <User@DESKTOP-CAEBGTR>
2020-04-20 23:45:51 +0700

    fixing delete users

--- auth/user/repository/repo_user.go
+++ auth/user/repository/repo_user.go
@@ -247,13 +247,13 @@ func (m *userRepository) Insert(ctx context.Context, a *models.User) error {
 }
 
 func (m *userRepository) Delete(ctx context.Context, id string, deleted_by string) error {
-	query := `UPDATE users SET deleted_by=? , deleted_date=? , is_deleted=? , is_active=?`
+	query := `UPDATE users SET deleted_by=? , deleted_date=? , is_deleted=? , is_active=? WHERE id =?`
 	stmt, err := m.Conn.PrepareContext(ctx, query)
 	if err != nil {
 		return err
 	}
 
-	_, err = stmt.ExecContext(ctx, deleted_by, time.Now(), 1, 0)
+	_, err = stmt.ExecContext(ctx, deleted_by, time.Now(), 1, 0,id)
 	if err != nil {
 		return err
 	}
