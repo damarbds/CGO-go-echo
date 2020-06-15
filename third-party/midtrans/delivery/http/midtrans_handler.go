529ed2bc297f08efe1efb0d044d060d22b5c0b2f
User <User@DESKTOP-CAEBGTR>
2020-06-15 22:09:15 +0700

    asd

--- third-party/midtrans/delivery/http/midtrans_handler.go
+++ third-party/midtrans/delivery/http/midtrans_handler.go
@@ -6171,7 +6171,7 @@ func (m *midtransHandler) MidtransNotif(c echo.Context) error {
 			departureTime, _ := time.Parse(layoutFormat, bookingDetail.Transportation[0].DepartureTime)
 			arrivalTime, _ := time.Parse(layoutFormat, bookingDetail.Transportation[0].ArrivalTime)
 
-			if bookingDetail.Transportation[0].ReturnTransId != nil && len(bookingDetail.Transportation) > 1 && len(bookingDetail.Transportation) > 1 {
+			if bookingDetail.Transportation[0].ReturnTransId != nil && len(bookingDetail.Transportation) > 1 {
 
 				bookingDetailReturn, err := m.bookingUseCase.GetDetailTransportBookingID(ctx, bookingDetail.OrderId, bookingDetail.OrderId, bookingDetail.Transportation[0].ReturnTransId)
 				if err != nil {
