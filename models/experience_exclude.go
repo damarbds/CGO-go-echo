d92551529162e33bd476e1b96061ab1925cb853c
User <User@DESKTOP-CAEBGTR>
2020-06-17 15:24:58 +0700

    migration experience exclude/facilities/include

---
+++ models/experience_exclude.go
@@ -0,0 +1,7 @@
+package models
+
+type ExperienceExclude struct {
+	Id 		int `json:"id"`
+	ExpId 	string `json:"exp_id"`
+	ExcludeId int `json:"exclude_id"`
+}
---
+++ models/experience_include.go
@@ -0,0 +1,7 @@
+package models
+
+type ExperienceInclude struct {
+	Id 			int 	`json:"id"`
+	ExpId 		string 	`json:"exp_id"`
+	IncludeId	int `json:"include_id"`
+}
